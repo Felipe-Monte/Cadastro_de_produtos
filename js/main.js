3cb51c9895a3f3311b5a22c6bcde0f146a17f7b1
Carlos Felipe <109633306+Felipe-Monte@users.noreply.github.com>
2023-05-21 08:56:24 -0300

    add product name and value

--- js/main.js
+++ js/main.js
@@ -2,32 +2,76 @@ const inputProduct = document.querySelector('#product')
 const inputPrice = document.querySelector('#price')
 const btnSave = document.querySelector('.save')
 
-const td = document.querySelectorAll('td')
+const tr = document.querySelectorAll('tbody tr')
 
 const Products = [
   {
     id: 1,
-    product: "Camisa",
+    product: "Joia",
     value: 90,
   },
   {
     id: 2,
-    product: "Blusa",
+    product: "Dado",
     value: 50,
   },
   {
     id: 3,
-    product: "Short",
+    product: "Livro",
     value: 36,
   }
 ]
+
+// function userData() {
+//   const tbody = document.querySelector('table tbody')
+
+//   Products.forEach(data => {
+//     const row = createTR()
+
+//     row.querySelector('.product_number').innerHTML = `${data.id}`
+//     row.querySelector('.product_name').innerHTML = `${data.product}`
+//     row.querySelector('.product_price').innerHTML = `${data.value}`
+
+//     tbody.append(row)
+//   })
+// }
+// userData()
+
+function createTR() {
+  let productName = inputProduct.value
+  let productPrice = Number(inputPrice.value)
+  const tr = document.createElement('tr')
+  tr.innerHTML = `
+  <td class="product_number">1</td>
+  <td class="product_name">${productName}</td>
+  <td class="product_price">${productPrice}</td>
+  <td class="table_button">
+    <button class="edit">Editar</button>
+    <button class="delete">Deletar</button>
+  </td>
+  `
+  return tr
+}
+
+function renderTable() {
+  let tbody = document.querySelector('table tbody')
+  let tr = createTR()
+  tbody.append(tr)
+}
+
 
 btnSave.addEventListener('click', () => {
   let productName = inputProduct.value
   let productPrice = Number(inputPrice.value)
 
+  if (!productName){
+    return alert('Product name is mandatory')
+  }
+  
+  renderTable()
 })
 
-td.forEach(data => {
-  console.log(data)
+tr.forEach(data => {
+  data.remove() 
 })
+
