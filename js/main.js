58bfbb1d625fae84c99c37910ad0d3bae1207cea
Carlos Felipe <109633306+Felipe-Monte@users.noreply.github.com>
2023-05-23 21:28:19 -0300

    add button delete

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 const inputProduct = document.querySelector('#product')
 const inputPrice = document.querySelector('#price')
+
 const btnSave = document.querySelector('.save')
 const btnCancel = document.querySelector('.cancel')
 
 const tr = document.querySelectorAll('tbody tr')
+let idCounter = 0
 
 const Products = [
   {
@@ -23,28 +25,12 @@ const Products = [
   }
 ]
 
-// function userData() {
-//   const tbody = document.querySelector('table tbody')
-
-//   Products.forEach(data => {
-//     const row = createTR()
-
-//     row.querySelector('.product_number').innerHTML = `${data.id}`
-//     row.querySelector('.product_name').innerHTML = `${data.product}`
-//     row.querySelector('.product_price').innerHTML = `${data.value}`
-
-//     tbody.append(row)
-//   })
-// }
-// userData()
-
-let id = 0
 function createTR() {
   let productName = inputProduct.value
   let productPrice = Number(inputPrice.value)
   const tr = document.createElement('tr')
   tr.innerHTML = `
-  <td class="product_number">${++id}</td>
+  <td class="product_number">${++idCounter}</td>
   <td class="product_name">${productName}</td>
   <td class="product_price">${productPrice}</td>
   <td class="table_button">
@@ -59,11 +45,15 @@ function renderTable() {
   let tbody = document.querySelector('table tbody')
   let tr = createTR()
   tbody.append(tr)
+
+  tr.querySelector('.delete').onclick = () => {
+    const isDelete = confirm('Deseja deletar')
+    if (!isDelete) return
+  }
 }
 
 btnSave.addEventListener('click', () => {
   let productName = inputProduct.value
-  let productPrice = Number(inputPrice.value)
 
   if (!productName) {
     return alert('Product name is mandatory')
