98a9fcbb0a02c6c4129c0b506b0026833cd98382
Carlos Felipe <109633306+Felipe-Monte@users.noreply.github.com>
2023-05-24 19:35:54 -0300

    new js

--- js/main.js
+++ js/main.js
@@ -4,60 +4,39 @@ const inputPrice = document.querySelector('#price')
 const btnSave = document.querySelector('.save')
 const btnCancel = document.querySelector('.cancel')
 
-const tr = document.querySelectorAll('tbody tr')
-
-let idCounter = 0
-
-let myItems = {}
-
-function createTR() {
-  let productName = inputProduct.value
-  let productPrice = Number(inputPrice.value)
-
-  const tr = document.createElement('tr')
-
-  tr.innerHTML = `
-  <td class="product_number">${++idCounter}</td>
-  <td class="product_name">${productName}</td>
-  <td class="product_price">${productPrice}</td>
-  <td class="table_button">
-    <button class="edit">Editar</button>
-    <button class="delete">Deletar</button>
-  </td>
-  `
-
-  myItems.name = productName
-  myItems.price = productPrice
-  // console.log(myItems)
-  localStorage.setItem("dadosTabela", JSON.stringify(myItems));
-  return tr
-}
-
-function renderTable() {
-  let tbody = document.querySelector('table tbody')
-  let tr = createTR()
-
-  tbody.append(tr)
-
-  tr.querySelector('.delete').onclick = () => {
-    const isDelete = confirm('Deseja deletar')
-    if (!isDelete) return
-  }
-}
+let items = []
 
 btnSave.addEventListener('click', () => {
+  const tbody = document.querySelector('table tbody')
   let productName = inputProduct.value
-
-  if (!productName) {
-    return alert('Product name is mandatory')
-  }
+  let productPrice = Number(inputPrice.value)
   
-  renderTable()
-})
+  let row = createTR()
+  items.push(row)
+  tbody.append(row)
+  console.log(items)
+})  
 
 btnCancel.addEventListener('click', () => {
   inputProduct.value = ''
   inputPrice.value = ''
   inputProduct.focus()
 })
+
+function createTR() {
+  const tr = document.createElement('tr')
+  let productName = inputProduct.value
+  let productPrice = Number(inputPrice.value)
+
+  tr.innerHTML = `
+  <td class="product_number">1</td>
+  <td class="product_name">${productName}</td>
+  <td class="product_price">${productPrice}</td>
+  <td class="table_button">
+    <button class="edit">Editar</button>
+    <button class="delete">Deletar</button>
+  </td>  
+  `
+  return tr
+}  
 
