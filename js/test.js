f1ca36007d8a720626f34363d7875b4b29be90a7
Carlos Felipe <109633306+Felipe-Monte@users.noreply.github.com>
2023-05-25 09:02:06 -0300

    update test js

---
+++ js/test.js
@@ -0,0 +1,8 @@
+
+
+function getUserDate() {
+  let product = {}
+
+  product.productName = document.querySelector('#product').value
+  product.productPrice = Number(document.querySelector('#price').value) 
+}
