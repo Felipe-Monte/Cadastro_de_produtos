e5d10a1d60edfda2049f362b7e3c9518d2cb4e1d
Carlos Felipe <109633306+Felipe-Monte@users.noreply.github.com>
2023-06-02 20:39:20 -0300

    add id counter

--- js/main.js
+++ js/main.js
@@ -6,18 +6,33 @@ const btnSave = document.querySelector('.save')
 
 const tbody = document.querySelector('table tbody')
 
-const data = []
-let id = 1
+let myList = JSON.parse(localStorage.getItem('itemList') || '[]')
+console.log(myList)
+
+let id = Number(localStorage.getItem('lastId') || '1')
 
 function render() {
-  let myList = JSON.parse(localStorage.getItem('itemList'))
-  console.log(myList)
+  for (let i = 0; i < myList.length; i++) {
+    let items = myList[i]
+    
+    let row = tbody.insertRow()
+
+    let td_id = row.insertCell()
+    let td_name = row.insertCell()
+    let td_price = row.insertCell()
+    let td_action = row.insertCell()
+
+    td_id.innerHTML = items.id
+    td_name.innerHTML = items.name
+    td_price.innerHTML = items.price
+    td_action.innerHTML = items.action
+  }
 }
 render()
 
 btnSave.addEventListener('click', function () {
   const products = getDataUser()
-  data.push(products)
+  myList.push(products)
   save()
 })
 
@@ -34,11 +49,12 @@ function getDataUser() {
   let td_price = row.insertCell()
   let td_action = row.insertCell()
 
+  products.id = id++
   products.name = productName
   products.price = productPrice
   products.action = '##'
 
-  td_id.innerHTML = id++
+  td_id.innerHTML = products.id
   td_name.innerHTML = products.name
   td_price.innerHTML = products.price
   td_action.innerHTML = products.action
@@ -47,5 +63,6 @@ function getDataUser() {
 }
 
 function save() {
-  localStorage.setItem('itemList', JSON.stringify(data))
+  localStorage.setItem('itemList', JSON.stringify(myList))
+  localStorage.setItem('lastId', id.toString())
 }
