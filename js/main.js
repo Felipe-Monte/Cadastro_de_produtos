c70aa56774cce6cada4796318037c16fcf6dbda6
Carlos Felipe <109633306+Felipe-Monte@users.noreply.github.com>
2023-06-01 22:52:35 -0300

    new project

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
 
+const inputProduct = document.querySelector('#product')
+const inputPrice = document.querySelector('#price')
+
+const btnSave = document.querySelector('.save')
+
+const data = []
+
+btnSave.addEventListener('click', function () {
+  const products = getDataUser()
+  data.push(products)
+
+  console.log(data)
+})
+
+function getDataUser() {
+  let products = {}
+
+  let productName = inputProduct.value
+  let productPrice = Number(inputPrice.value)
+
+  products.name = productName
+  products.price = productPrice
+  products.action = 'action were'
+
+  return products
+}
