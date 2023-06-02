89e94a09e1a2ac0ecc840d8a79ebf77d41bcb360
Carlos Felipe <109633306+Felipe-Monte@users.noreply.github.com>
2023-06-01 23:02:13 -0300

    add get user data

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ const inputPrice = document.querySelector('#price')
 
 const btnSave = document.querySelector('.save')
 
+const tbody = document.querySelector('table tbody')
+
 const data = []
 
 btnSave.addEventListener('click', function () {
@@ -19,9 +21,21 @@ function getDataUser() {
   let productName = inputProduct.value
   let productPrice = Number(inputPrice.value)
 
+  let row = tbody.insertRow()
+
+  let td_id = row.insertCell()
+  let td_name = row.insertCell()
+  let td_price = row.insertCell()
+  let td_action = row.insertCell()
+
   products.name = productName
   products.price = productPrice
   products.action = 'action were'
+
+  td_id.innerHTML = '0'
+  td_name.innerHTML = products.name
+  td_price.innerHTML = products.price
+  td_action.innerHTML = products.action
 
   return products
 }
